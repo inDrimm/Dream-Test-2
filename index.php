cc44aefc29c5f4ffc6dabef23536745dcdc95cb9
Muhamad Ilham Hidayat <muhamadilhamhidayat24@gmail.com>
2020-08-03 22:50:23 +0700

    menyimpan index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Test Remote 2</title>
+</head>
+
+<body>
+  <h1>Hello remote</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit officiis aperiam aut quae repellendus quis nulla consequatur, enim excepturi, numquam libero quasi, recusandae officia mollitia ipsa. Exercitationem eaque corporis soluta.</p>
+</body>
+
+</html>
